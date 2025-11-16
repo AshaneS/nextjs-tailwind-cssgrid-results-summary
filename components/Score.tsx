2554dbd3bd48fc597071850c1e7a84199d958397
Ashane Silva <ashanecool@gmail.com>
2025-11-16 22:57:43 +0530

    fixed ScoreItem score type to number

--- components/Score.tsx
+++ components/Score.tsx
@@ -1,9 +1,8 @@
-"use client";
 import Image from "next/image";
 
 interface ScoreItem {
-  category: "Reaction" | "Memory" | "Verbal" | "Visual";
-  score: string;
+  category: string;
+  score: number;
   icon: string;
 }
 
